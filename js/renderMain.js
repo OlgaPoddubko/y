8a2485301f073b338d0e79255018bd047c7e25c8
Olga <olgapoddubko@gmail.com>
2017-05-20 20:23:22 +0300

    implement swipe and multipaging

--- js/renderMain.js
+++ js/renderMain.js
@@ -71,9 +71,11 @@ function renderMain(resp) {
             '</div>' +
             '</div>' +
             '<div class="paging">' +
-            '<span class="page prev"></span>' +
-            '<span class="page curr"></span>' +
-            '<span class="page next"></span>' +
+            '<span class="page prev-three"><span class="tooltip"></span></span>' +
+            '<span class="page prev-two"><span class="tooltip"></span></span>' +
+            '<span class="page prev-one"><span class="tooltip"></span></span>' +
+            '<span class="page curr"><span class="tooltip"></span></span>' +
+            '<span class="page next"><span class="tooltip"></span></span>' +
             '</div>';
   }
 
@@ -83,23 +85,103 @@ function renderMain(resp) {
   addSection(resp);
 
 // pagination
-  const mainInner = document.body.querySelector('main-inner');
-  console.log(mainInner);
-  /* const mainInnerWidth = getComputedStyle(mainInner);
-  console.log(mainInnerWidth);*/
+  const sectionWidth = 350;
 
+  const mainInner = document.querySelector('.main-inner');
+  const mainInnerWidth = global.getComputedStyle(mainInner).width;
 
-  const width = 350;
-  const columns = 4;
+  let columns;
+  switch (mainInnerWidth) {
+    case '1400px': columns = 4;
+      break;
+    case '1030px': columns = 3;
+      break;
+    case '680px': columns = 2;
+      break;
+    default: columns = 1;
+      break;
+  }
+
   let position = 0;
-  const gallery = document.querySelector('.gallery');
+  const gallery = document.body.querySelector('.gallery');
+
   let currentPageNumber = 1;
 
+  const prevOne = document.querySelector('.prev-one');
+  const prevTwo = document.querySelector('.prev-two');
+  const prevThree = document.querySelector('.prev-three');
+
+  function checkPageItemsNumber() {
+    if (currentPageNumber === 1) {
+      prevOne.style.display = 'none';
+      prevTwo.style.display = 'none';
+      prevThree.style.display = 'none';
+    }
+
+    if (currentPageNumber === 2) {
+      prevOne.style.display = 'inline-block';
+      prevTwo.style.display = 'none';
+      prevThree.style.display = 'none';
+    }
+    if (currentPageNumber === 3) {
+      prevOne.style.display = 'inline-block';
+      prevTwo.style.display = 'inline-block';
+      prevThree.style.display = 'none';
+    }
+
+    if (currentPageNumber >= 4) {
+      prevOne.style.display = 'inline-block';
+      prevTwo.style.display = 'inline-block';
+      prevThree.style.display = 'inline-block';
+    }
+  }
+  checkPageItemsNumber();
+
+  const pageItems = document.querySelectorAll('.page');
+
+  const currTt = document.querySelector('.curr  > .tooltip');
+  const nextTt = document.querySelector('.next  > .tooltip');
+  const prevOneTt = document.querySelector('.prev-one > .tooltip');
+  const prevTwoTt = document.querySelector('.prev-two  > .tooltip');
+  const prevThreeTt = document.querySelector('.prev-three  > .tooltip');
+
+  currTt.innerHTML = currentPageNumber;
+
+  function hidePageNumber(e) {
+    const target = e.target;
+    target.style.visibility = 'hidden';
+
+    target.removeEventListener('mouseup', hidePageNumber);
+  }
+
+  function showPageNumber(e) {
+    const target = e.target;
+
+    if (target.matches('.tooltip')) {
+      console.log('tooltip');
+      return;
+    }
+
+    const tooltip = target.querySelector('.tooltip');
+    tooltip.style.visibility = 'visible';
+    target.addEventListener('mouseup', hidePageNumber);
+  }
+
+  pageItems.forEach(item => item.addEventListener('mousedown', showPageNumber));
+
   function pagePrev() {
-    position = Math.min(position + (width * columns), 0);
+    position = Math.min(position + (sectionWidth * columns), 0);
     gallery.style.marginLeft = `${position}px`;
     if (currentPageNumber > 1) {
       currentPageNumber -= 1;
+
+      currTt.innerHTML = currentPageNumber;
+      nextTt.innerHTML = currentPageNumber + 1;
+      prevOneTt.innerHTML = currentPageNumber - 1;
+      prevTwoTt.innerHTML = currentPageNumber - 2;
+      prevThreeTt.innerHTML = currentPageNumber - 3;
+
+      checkPageItemsNumber();
     }
   }
 
@@ -114,13 +196,61 @@ function renderMain(resp) {
         console.warn(error);
       });
     }
-    position -= width * columns;
+
+    position -= sectionWidth * columns;
     gallery.style.marginLeft = `${position}px`;
 
     currentPageNumber += 1;
+
+    currTt.innerHTML = currentPageNumber;
+    nextTt.innerHTML = currentPageNumber + 1;
+    prevOneTt.innerHTML = currentPageNumber - 1;
+    prevTwoTt.innerHTML = currentPageNumber - 2;
+    prevThreeTt.innerHTML = currentPageNumber - 3;
+
+    checkPageItemsNumber();
   }
 
-// pagination with mouse
+  function changePage(e) {
+    let coefficient;
+    console.log(e.target);
+
+    if (e.target.matches('.next')) {
+      coefficient = 1;
+    } else if (e.target.matches('.curr')) {
+      return;
+    } else if (e.target.matches('.prev-one')) {
+      coefficient = -1;
+    } else if (e.target.matches('.prev-two')) {
+      coefficient = -2;
+    } else if (e.target.matches('.prev-three')) {
+      coefficient = -3;
+    }
+
+    if ((currentPageNumber + 2) * columns * coefficient > itemsNumber) {
+      service.downloadMore(nextPageToken, query).then((response) => {
+        addSection(response);
+      }).catch((error) => {
+        console.warn(error);
+      });
+    }
+
+    position -= sectionWidth * columns * coefficient;
+    gallery.style.marginLeft = `${position}px`;
+
+    currentPageNumber += (1 * coefficient);
+
+    currTt.innerHTML = currentPageNumber;
+    nextTt.innerHTML = currentPageNumber + 1;
+    prevOneTt.innerHTML = currentPageNumber - 1;
+    prevTwoTt.innerHTML = currentPageNumber - 2;
+    prevThreeTt.innerHTML = currentPageNumber - 3;
+
+    checkPageItemsNumber();
+  }
+
+
+// pagination with mousemove
   let drag = false;
   let currentDrag = 0;
 
@@ -144,13 +274,43 @@ function renderMain(resp) {
     drag = false;
   }
 
-  const paging = document.querySelector('.paging');
-  paging.querySelector('.prev').addEventListener('click', pagePrev);
-  paging.querySelector('.next').addEventListener('click', pageNext);
+  pageItems.forEach(item => item.addEventListener('click', changePage));
 
-  document.body.addEventListener('mousedown', mousedown);
-  document.body.addEventListener('mousemove', mousemove);
-  document.body.addEventListener('mouseup', mouseup);
+  mainInner.addEventListener('mousedown', mousedown);
+  mainInner.addEventListener('mousemove', mousemove);
+  mainInner.addEventListener('mouseup', mouseup);
+
+  let xDown = null;
+  let yDown = null;
+  function handleTouchStart(e) {
+    xDown = e.touches[0].clientX;
+    yDown = e.touches[0].clientY;
+  }
+  function handleTouchMove(e) {
+    if (!xDown || !yDown) {
+      return;
+    }
+
+    const xUp = e.touches[0].clientX;
+    const yUp = e.touches[0].clientY;
+
+    const xDiff = xDown - xUp;
+    const yDiff = yDown - yUp;
+    if (Math.abs(xDiff) + Math.abs(yDiff) > 150) {
+      if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+          pageNext();
+        } else {
+          pagePrev();
+        }
+      }
+      xDown = null;
+      yDown = null;
+    }
+  }
+
+  mainInner.addEventListener('touchstart', handleTouchStart, false);
+  mainInner.addEventListener('touchmove', handleTouchMove, false);
 }
 
 module.exports.renderMain = renderMain;
